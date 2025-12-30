10977bea9a322e2bf1cc7f446ee74ea6bd4fef14
jayantsingh924 <jayantsingh924@gmail.com>
2025-12-30 14:23:03 +0530

    Added Categories Controller to create new Categories with Dto

---
+++ API/DearDiary.API/DearDiary.API/Controllers/CategoriesController.cs
@@ -0,0 +1,44 @@
+﻿using DearDiary.API.Data;
+using DearDiary.API.Models.Domain;
+using DearDiary.API.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DearDiary.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public CategoriesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            // Implementation for creating a category will go here.
+            //Map DTO to Domain Model
+            
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+            await dbContext.Categories.AddAsync(category);
+            await dbContext.SaveChangesAsync();
+
+            //Domain Model to DTO
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+            return Ok(response);
+
+        }
+    }
+}
---
+++ API/DearDiary.API/DearDiary.API/Models/DTO/CreateCategoryRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DearDiary.API.Models.DTO
+{
+    public class CreateCategoryRequestDto
+    {
+        public string Name { get; set; }
+        public string UrlHandle { get; set; }
+    }
+}
